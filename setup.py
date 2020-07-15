13ea6c250a82bf00db84651fad02aca7b340d628
Thamme Gowda <tg@isi.edu>
2020-07-15 15:09:41 -0700

    add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,49 @@
+import setuptools
+from pathlib import Path
+import re
+
+long_description = Path('README.md').read_text(encoding='utf-8', errors='ignore')
+
+
+vpat = re.compile(r"""__version__\s*=\s*['"]([^'"]*)['"]""")
+__version__ = None
+for line in Path('unmass/__init__.py').read_text().splitlines():
+    line = line.strip()
+    if vpat.match(line):
+        __version__ = vpat.match(line)[1]
+
+print(f"Going to install rtg {__version__}")
+assert __version__, 'Could not find __version__ in __init__.py'
+
+setuptools.setup(
+    name='rtg',
+    version=__version__,
+    author="",
+    author_email="",
+    description="UNMASS - Unsupervised NMT based on Masked Sequence-to-Sequence ",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        'License :: OSI Approved :: ',
+        "Operating System :: OS Independent",
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    platforms=['any'],
+    install_requires=[
+        'ruamel.yaml >= 0.16.10',
+        'sacrebleu >= 1.4.6',
+        'sacremoses >= 1.0.0',
+        'nlcodec >= 0.2.3',
+        'torch >= 1.4'
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'unmass-train=unmass.train:main',
+        ],
+    }
+)
